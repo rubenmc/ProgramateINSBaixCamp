72259af68f6fecca4d6ec95623e21211dcd25e17
Alejandro Samper <alejandro.samper@insbaixcamp.cat>
2013-03-12 18:00:06 +0100

    Cerillas OK

--- src/Cerillas.java
+++ src/Cerillas.java
@@ -1,36 +1,32 @@
 import java.util.Scanner;
 
+
 public class Cerillas {
 
-	/**
-	 * Da RUN-ERROR
-	 * Tanto en lectura infinita como en lectura hasta perder da error
-	 * Estaria bien saber el caso en el que falla
-	 */
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num, max;
-		boolean sortida = false;
-		// Bucle infinito mientras nos metan datos
-		while (!sortida) {
-			// recumeramos valores
-			num = Integer.parseInt(scan.next());
-			max = Integer.parseInt(scan.next());
-			//Si solo queda una perdemos
-			if (num == 1) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		int cerillas, mano, result;
+		
+		while(keyboard.hasNextInt()){
+			cerillas = keyboard.nextInt();
+			mano = keyboard.nextInt();
+			result = cerillas - calcul(cerillas, mano);
+			if (result == 0)
 				System.out.println("PIERDO");
-				sortida = true;
-			} else {
-				//Si quedan mas que el maximo + 1 retiramos solo 1
-				if (num > max + 1) {
-					System.out.println(1);
-				//Si quedan el maximo + 1 o menos retiramos todas menos 1
-				} else {
-					System.out.println(num - 1);
-				}
-			}
+			else
+				System.out.println(result);
 		}
-		scan.close();
+		keyboard.close();
+
+	}
+
+	static int calcul(int cerillas, int mano) {
+		int res = 1;
+		while (res <= cerillas) {
+			res += mano+1;
+		}
+		return res-=mano+1;
 	}
 
 }
