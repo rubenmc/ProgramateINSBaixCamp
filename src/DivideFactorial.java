f916366293db362f6ff55f2b12ee7e026c42dad7
Ruben Macias <ruben.macias@insbaixcamp.cat>
2013-03-12 17:20:27 +0100

    Modify DivideFactiral.java, last version

--- src/DivideFactorial.java
+++ src/DivideFactorial.java
@@ -1,14 +1,9 @@
 import java.util.Scanner;
 /**
  * 
- * @author Ruben
  * Divide el factorial del segundo numero por el primero y devuelve YES si el residuo es 0 o NO si el residuo es diferente a 0
  * 
- * 
- * A terminar:
- * 
- * Da Run Error
- * Comprovar si el primero es primo?
+ *Correcto en el juez
  *
  */
 public class DivideFactorial {
@@ -17,21 +12,17 @@ public class DivideFactorial {
 		Scanner scan = new Scanner(System.in);
 		int p = Integer.parseInt(scan.next());
 		int n = Integer.parseInt(scan.next());
-		int res;
 		boolean sortida = false;
 		while (!sortida) {
 			if (!(p<0 && n<0)) {//Comprovamos si los numeros son positivos
-				//Inicializamos res
-				res = 1;
-				if (n != 0) { //Si es 0 dejamos a res en 1, si no, calculamos el factorial
-					for (int x = 1; x < n; x++) {
-						res = res * x;
-					}
+				if(n==0){ //Si el numero es 0 lo cambiamos a 1
+					n=1;
 				}
-				//Si la division no da residuo imprimimos YES
-				if (res % p == 0) {
+				if(p<=n){
 					System.out.println("YES");
-				} else { //Si la division da residuo imprimimos NO
+				} else if (n%p==0){
+					System.out.println("YES");
+				} else {
 					System.out.println("NO");
 				}
 				//Recogemos los nuevos casos
