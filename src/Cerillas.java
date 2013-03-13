c623ea05b853dd09fed904300485b3f14e0b1818
Draguns <alejandro.samper@insbaixcamp.cat>
2013-03-14 00:00:00 +0100

    Las 15 Cerillas - Final Version. Judge OK

--- src/Cerillas.java
+++ src/Cerillas.java
@@ -5,12 +5,12 @@ public class Cerillas {
 
 	public static void main(String[] args) {
 		
-		Scanner keyboard = new Scanner(System.in);
-		int cerillas, mano, result;
+		Scanner keyboard = new Scanner(System.in); //Scanner
+		int cerillas, mano, result; //Variables
 		
-		while(keyboard.hasNextInt()){
-			cerillas = keyboard.nextInt();
-			mano = keyboard.nextInt();
+		while(keyboard.hasNextInt()){ //Mientras keden datos
+			cerillas = keyboard.nextInt(); //leemos cerillas totales
+			mano = keyboard.nextInt(); //leemos cerillas que podemos quitar
 			result = cerillas - calcul(cerillas, mano);
 			if (result == 0)
 				System.out.println("PIERDO");
@@ -23,10 +23,10 @@ public class Cerillas {
 
 	static int calcul(int cerillas, int mano) {
 		int res = 1;
-		while (res <= cerillas) {
-			res += mano+1;
+		while (res <= cerillas) { //Partimos de 1 (la ultima cerilla, y le sumamos...
+			res += mano+1; 			// ...la jugada perfecta -> la mano +1, hasta llegar al total
 		}
-		return res-=mano+1;
+		return res-=mano+1;		// hay k eliminar la ultima iteracion que pasa del total
 	}
 
 }
